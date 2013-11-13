73e237b62c2d3113c943f8e53481d08515e4513f
Karl Sanford <ksanford@ostusa.com>
2013-11-12 20:31:58 -0500

    Added data model and a base review controller

---
+++ DoesItSuck/DoesItSuck/Controllers/ReviewController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoesItSuck.Models;
+
+namespace DoesItSuck.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private DoesItSuckEntities db = new DoesItSuckEntities();
+
+        //
+        // GET: /Review/
+
+        public ActionResult Index()
+        {
+            return View(db.Review.ToList());
+        }
+
+        //
+        // GET: /Review/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Review review = db.Review.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        //
+        // GET: /Review/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Review/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Review.Add(review);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(review);
+        }
+
+        //
+        // GET: /Review/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Review review = db.Review.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        //
+        // POST: /Review/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(review).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(review);
+        }
+
+        //
+        // GET: /Review/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Review review = db.Review.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        //
+        // POST: /Review/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Review review = db.Review.Find(id);
+            db.Review.Remove(review);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
