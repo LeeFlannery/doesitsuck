a2a0982801a31823fa94ff0b4942c7d13a8d0ffd
tom-fish-ost <tfish@ostusa.com>
2013-11-19 19:41:50 -0500

    LULZ

--- DoesItSuck/DoesItSuck/Models/Category.cs
+++ DoesItSuck/DoesItSuck/Models/Category.cs
@@ -7,11 +7,11 @@ namespace DoesItSuck.Models
 {
     public enum Category
     {
-        Other = 0,
         Games = 1,
         Tech = 2,
         Music = 3,
         Video = 4,
-        Movies = 5
+        Movies = 5,
+        LULZ = 6
     }
 }
